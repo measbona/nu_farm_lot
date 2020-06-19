c8cf7e79fb551d086c2ccae5bfa681518ca118be
MEASBona <bonameas@MEASBonas-MacBook-Pro.local>
2020-06-19 17:05:00 +0700

    [INTERGRATE/Styled-Components] Install Styled Components V5

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import styled from 'styled-components';
+
+const View = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Text = styled.Text``;
 
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View>
         <Text>Hello World</Text>
         <Icon name="rocket" size={30} color="#900" />
       </View>
