76fdd1dae54a5d68a8e1dd54e8c332990045e5ba
MEASBona <bonameas@MEASBonas-MacBook-Pro.local>
2020-06-19 16:55:28 +0700

    [INTERGRATE/RNVectorIcon ]Install React Native Vector Icons V6

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Dashboard extends React.Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Hello World</Text>
+        <Icon name="rocket" size={30} color="#900" />
       </View>
     );
   }
